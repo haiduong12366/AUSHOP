97763bda81a6d27344714704f701cc7b7e4ec5eb
Hai Duong <ilabt01236@gmail.com>
2023-12-05 22:55:31 +0700

    done 5/12

--- AUSHOP/src/main/java/AUSHOP/repository/KhachHangRepository.java
+++ AUSHOP/src/main/java/AUSHOP/repository/KhachHangRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import AUSHOP.entity.KhachHang;
 
-public interface KhachHangRepository extends JpaRepository<KhachHang,Integer> {
+public interface KhachHangRepository extends JpaRepository<KhachHang,Long> {
 
 	Optional<KhachHang> findByEmail(String name);
 	
