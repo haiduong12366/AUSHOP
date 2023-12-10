3bcaa7c8dbdb0b4db861315cd16a6cf9bc4f16c1
Bùi Thiện <21110656@student.hcmute.edu.vn>
2023-12-10 22:52:36 +0700

    trangchu

--- AUSHOP/src/main/java/AUSHOP/Controllers/HomeController.java
+++ AUSHOP/src/main/java/AUSHOP/Controllers/HomeController.java
@@ -1,5 +1,7 @@
 package AUSHOP.Controllers;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,6 +32,9 @@ import AUSHOP.repository.SanPhamRepository;
 import AUSHOP.repository.UserRoleRepository;
 import AUSHOP.services.ShoppingCartService;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -50,6 +56,99 @@ public class HomeController {
 	@Autowired
 	UserRoleRepository userRoleRepository;
 
+	@RequestMapping(value = {"/home"})
+	public ModelAndView home(ModelMap model, Principal principal) {
+		boolean isLogin = false;
+		if (principal != null) {
+			isLogin = true;
+		}
+		model.addAttribute("isLogin", isLogin);
+
+		if(principal!=null) {
+			Optional<KhachHang> c = customerRepository.findByEmail(principal.getName());
+			Optional<UserRole> uRole = userRoleRepository.findByMaKhachHang(Integer.valueOf(c.get().getMaKhachHang()));
+			if(uRole.get().getRoleId().getTen().equals("ROLE_ADMIN")) {
+				return new ModelAndView("forward:/admin/customers", model);
+			}
+		}
+
+		List<SanPham> list8Last = productRepository.get8Last();
+		List<SanPham> listtop4 = productRepository.gettop4();
+//		List<SanPham> listnext4 = productRepository.getNext4(1);
+		List<Object[]> listbest = productRepository.getTopProducts(PageRequest.of(0, 4));
+
+		model.addAttribute("products", list8Last);
+		model.addAttribute("products_top4", listtop4);
+		model.addAttribute("products_banchay", listbest);
+		model.addAttribute("slide", true);
+		return new ModelAndView("/site/index-home", model);
+	}
+
+	@RequestMapping(value = {"/load"}, method= RequestMethod.GET)
+	protected void load(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		String amount = request.getParameter("exits");
+		int iamount = Integer.parseInt(amount);
+		List<SanPham> list = productRepository.getNext4(iamount);
+		PrintWriter out = response.getWriter();
+
+		for (SanPham item : list) {
+			out.println("<div class=\"product col-lg-3 col-md-6 mb-4\">\n"
+					+ "						<div class=\"card h-100\">\n"
+					+ "							<a th:href=\"@{'/shop/item/'"+item.getMaSP()+"}\"><img class=\"card-img-top\"\n"
+					+ "									th:src=\"@{'/getproduct/'"+item.getHinhAnh()+"}\" alt=\"...\" /></a>\n"
+					+ "							<div class=\"card-body\">\n"
+					+ "								<h4 class=\"card-title text-center\">\n"
+					+ "									<a th:href=\"@{'/shop/item/'"+item.getMaSP()+"}\" th:text=\""+item.getTenSP()+"\"></a>\n"
+					+ "								</h4>\n"
+					+ "								<p class=\"text-center\">\n"
+					+ "									<small>(còn [["+item.getSlTonKho()+"]] sản phẩm)</small>\n"
+					+ "								</p>\n"
+					+ "								<p class=\"card-text text-center\">Bàn phím cơ chơi game. Sự lựa\n"
+					+ "									chọn tuyệt vời.</p>\n"
+					+ "							</div>\n"
+					+ "							<a class=\"card-footer text-center\" th:href=\"@{'/addCart/'"+item.getMaSP()+"}\"\n"
+					+ "								style=\"cursor: pointer; text-decoration: none; color: gray;\"> <span\n"
+					+ "									style=\"margin: auto;\">Thêm vào giỏ hàng <i class=\"fa fa-shopping-cart\"></i></span>\n"
+					+ "							</a>\n"
+					+ "						</div>\n"
+					+ "					</div>");
+		}
+	}
+
+	@RequestMapping(value = {"/load"}, method=RequestMethod.POST)
+	public void loadPost(HttpServletRequest request, HttpServletResponse response)throws IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		String amount = request.getParameter("exits");
+		int iamount = Integer.parseInt(amount);
+		List<SanPham> list = productRepository.getNext4(iamount);
+		PrintWriter out = response.getWriter();
+
+		for (SanPham item : list) {
+			out.println("<div class=\"product col-lg-3 col-md-6 mb-4\">\n"
+					+ "						<div class=\"card h-100\">\n"
+					+ "							<a th:href=\"@{'/shop/item/'"+item.getMaSP()+"}\"><img class=\"card-img-top\"\n"
+					+ "									th:src=\"@{'/getproduct/'"+item.getHinhAnh()+"}\" alt=\"...\" /></a>\n"
+					+ "							<div class=\"card-body\">\n"
+					+ "								<h4 class=\"card-title text-center\">\n"
+					+ "									<a th:href=\"@{'/shop/item/'"+item.getMaSP()+"}\" th:text=\""+item.getTenSP()+"\"></a>\n"
+					+ "								</h4>\n"
+					+ "								<p class=\"text-center\">\n"
+					+ "									<small>(còn [["+item.getSlTonKho()+"]] sản phẩm)</small>\n"
+					+ "								</p>\n"
+					+ "								<p class=\"card-text text-center\">Bàn phím cơ chơi game. Sự lựa\n"
+					+ "									chọn tuyệt vời.</p>\n"
+					+ "							</div>\n"
+					+ "							<a class=\"card-footer text-center\" th:href=\"@{'/addCart/'"+item.getMaSP()+"}\"\n"
+					+ "								style=\"cursor: pointer; text-decoration: none; color: gray;\"> <span\n"
+					+ "									style=\"margin: auto;\">Thêm vào giỏ hàng <i class=\"fa fa-shopping-cart\"></i></span>\n"
+					+ "							</a>\n"
+					+ "						</div>\n"
+					+ "					</div>");
+		}
+	}
 	@RequestMapping(value = {"/shop"})
 	public ModelAndView shop(ModelMap model, Principal principal) {
 		boolean isLogin = false;
